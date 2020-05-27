53767e7e3ddb866b9dbf7eccbf60a4c5c5131306
Metehan Gulac <metehanglc@protonmail.com>
2020-05-27 22:56:16 +0300

    plugin eklendi.

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,8 @@ export default {
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxt/content'
   ],
   /*
    ** Axios module configuration
