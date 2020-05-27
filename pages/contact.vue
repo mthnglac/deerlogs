14b65891ed3224778568ca82407e32d0ff307f93
Metehan Gulac <metehanglc@protonmail.com>
2020-05-27 22:44:04 +0300

    added context

--- pages/contact.vue
+++ pages/contact.vue
@@ -35,8 +35,10 @@
             <v-spacer />
             <v-card-text class="custom-text-zone">
               <div>
-                If you have any questions, please don't hesitate to contact
-                using form below...
+                I am interested in freelance opportunities - especially
+                ambitious or large projects. However, if you have other request
+                or question, don't hesitate to contact me using below form
+                either.
               </div>
             </v-card-text>
           </v-col>
